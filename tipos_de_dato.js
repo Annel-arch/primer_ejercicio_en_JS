23e5c12579c4942fdee7fcfb03552489649278c5
annel_yopla <annelyoplap@gmail.com>
2024-12-18 16:10:53 -0500

    erminando el ejercicio

--- tipos_de_dato.js
+++ tipos_de_dato.js
@@ -1,2 +1,9 @@
 console.log (typeof 125)
-
+console.log (typeof 'Veinticinco')
+console.log (typeof -666)
+console.log (typeof true)
+console.log (typeof 0)
+console.log (typeof '')
+console.log (typeof null)
+console.log (typeofundefined)
+console.log (typeof FALSE)
